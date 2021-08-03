9a3e419a4519306d32aff616fbec0d6ad3df608c
Salman Malik <salmanmalik@Salmans-MBP.attlocal.net>
2021-08-03 15:42:35 -0500

    cleaned up readme

--- src/components/Background.js
+++ src/components/Background.js
@@ -24,7 +24,7 @@ export default function Background() {
             <div >
                 <img style={{ height: "428px", width: "20%", position: "absolute", backgroundSize: "contain", backgroundRepeat: "no-repeat" }} src="https://d2d8wwwkmhfcva.cloudfront.net/1500x1520/d2guulkeunn7d8.cloudfront.net/assets/homepage/homepage-mobile-background-m3-high-res-ca3279087ea0a3337f671b694b196d43fd207443d5825e61295b0996974835e9.jpg" alt="Card image" />
             </div>
-            {/* // if you're trying to use the Card Image Overlays, this is how your code could look instead of the style above - both are acceptable, the above example is a better replication and is also cleaner! */}
+            {/* // if you're wanting to use the Card Image Overlays, this is how your code could look instead of the style above - both are acceptable, the above example is a better replication and is also cleaner! */}
             {/* <Card style={{ width: "80%", minHeight: "428px" }} className="bg-dark text-black"> */}
             {/* <Card.Img style={{ height: "100%", width: "100%", position: "absolute" }} src="https://d2d8wwwkmhfcva.cloudfront.net/1500x1520/d2guulkeunn7d8.cloudfront.net/assets/homepage/homepage-mobile-background-m3-high-res-ca3279087ea0a3337f671b694b196d43fd207443d5825e61295b0996974835e9.jpg" alt="Card image" />
                 <Card.ImgOverlay style={{ top: "auto", width: "578px", marginLeft: "100px", marginBottom: "50px"}}>
